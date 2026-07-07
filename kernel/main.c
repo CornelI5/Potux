c9aba7df5a0d9f80206bd035c64761c9e25b733f
CornelI5 <kenzoyooo0@gmail.com>
2026-07-07 15:50:31 +0700

    Update main.c

--- kernel/main.c
+++ kernel/main.c
@@ -1,21 +1,18 @@
 #include "../include/vga.h"
+#include "../include/game.h"
 
 extern void init_idt();
 extern void remap_pic();
-extern void draw_line(int x0, int y0, int x1, int y1, unsigned char color);
-extern void clear_screen(unsigned char color);
+extern void init_timer(uint32_t freq);
 
 void kmain(void) {
     remap_pic();
     init_idt();
     
+    init_timer(30); 
+
     vga_init();
-    clear_screen(0x00);
-    
-    draw_line(0, 0, 319, 0, 0x0F); 
-    draw_line(0, 0, 0, 199, 0x0F); 
-    draw_line(319, 0, 319, 199, 0x0F); 
-    draw_line(0, 199, 319, 199, 0x0F); 
+    init_game(); 
 
     __asm__ volatile ("sti");
     while(1) {
