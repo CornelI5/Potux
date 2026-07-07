6c61420d4916b00a598c8d348b3a0d6c4fb68f3f
CornelI5 <kenzoyooo0@gmail.com>
2026-07-07 15:39:23 +0700

    Update main.c

--- kernel/main.c
+++ kernel/main.c
@@ -1,23 +1,24 @@
-#include "../include/io.h"
-#include "../include/keyboard.h"
+#include "../include/vga.h"
 
 extern void init_idt();
 extern void remap_pic();
+extern void draw_line(int x0, int y0, int x1, int y1, unsigned char color);
+extern void clear_screen(unsigned char color);
 
 void kmain(void) {
-    const char *welcome = "Welcome to POTUX v0.1 (Unix-Linux Fusion)";
-    char *vga = (char*)0xB8000;
-    for(int i = 0; welcome[i] != '\0'; i++) {
-        vga[i*2] = welcome[i];
-        vga[i*2+1] = 0x0F;
-    }
-
     remap_pic();
     init_idt();
+    
+    vga_init();
+    clear_screen(0x00);
+    
+    draw_line(0, 0, 319, 0, 0x0F); 
+    draw_line(0, 0, 0, 199, 0x0F); 
+    draw_line(319, 0, 319, 199, 0x0F); 
+    draw_line(0, 199, 319, 199, 0x0F); 
 
     __asm__ volatile ("sti");
-
     while(1) {
-        __asm__ volatile ("hlt"); 
+        __asm__ volatile ("hlt");
     }
 }
