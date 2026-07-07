0457137c98262763c0ad9d7d151d2f1ac0b652f0
CornelI5 <kenzoyooo0@gmail.com>
2026-07-07 15:53:26 +0700

    Update isr.c

--- kernel/isr.c
+++ kernel/isr.c
@@ -1,7 +1,27 @@
-void timer_callback(registers_t regs) {
-    static uint32_t tick = 0;
-    tick++;
+#include "../include/io.h"
+#include "../include/game.h" 
 
-    update_game_physics();
-    render_game_frame();
+
+typedef struct {
+    unsigned int ds;                                    
+    unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax; 
+    unsigned int int_no, err_code;                      
+    unsigned int eip, cs, eflags, useresp, ss;           
+
+void interrupt_handler(registers_t regs) {
+    
+    if (regs.int_no == 32) {
+        update_game_physics();
+        render_game_frame();
+        
+        outb(0x20, 0x20);
+    }
+    
+    else if (regs.int_no == 33) {
+        unsigned char scancode = inb(0x60);
+        
+        handle_game_input(scancode);
+        
+        outb(0x20, 0x20);
+    }
 }
