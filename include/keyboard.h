3e81e438816f6cbcc6b2534cc3cf4b719d77cb7a
CornelI5 <kenzoyooo0@gmail.com>
2026-07-07 15:10:18 +0700

    Update keyboard.h

--- include/keyboard.h
+++ include/keyboard.h
@@ -1,7 +1,22 @@
 #ifndef _KEYBOARD_H
 #define _KEYBOARD_H
 
+#include "io.h" 
+
+#define KBD_DATA_PORT 0x60
+#define KBD_CTRL_PORT 0x64
+
+#define KBD_BACKSPACE 0x0E
+#define KBD_ENTER     0x1C
+
 void init_keyboard();
-void keyboard_handler(); 
+void keyboard_handler();
+
+static const char scancode_map[] = {
+    0, 0, '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 0,
+    0, 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', 0,
+    0, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', '`', 0,
+    '\\', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 0, 0, 0, ' '
+};
 
 #endif
